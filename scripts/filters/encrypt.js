966f3e01812591abe1cc8f855e2b5b81d58ab959
hans362 <i@hans362.cn>
2025-04-11 14:23:09 +0800

    use after_post_render instead of tag for encryption

--- scripts/filters/encrypt.js
+++ scripts/filters/encrypt.js
@@ -1,6 +1,42 @@
 "use strict";
+const crypto = require("crypto");
+const cheerio = require("cheerio");
+
+function aes_encrypt(key, iv, msg) {
+  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+  let encrypted = cipher.update(msg, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return encrypted;
+}
+
+function sha384(msg) {
+  const hash = crypto.createHash("sha384");
+  hash.update(msg);
+  return hash.digest("hex");
+}
 
 hexo.extend.filter.register("after_post_render", (data) => {
+  const $ = cheerio.load(data.content);
+  $("encrypt").each((i, elem) => {
+    const password = elem.attribs.password;
+    const hint = elem.attribs.hint;
+    const hash = Buffer.from(sha384(password), "hex");
+    const key = hash.subarray(0, 32);
+    const iv = hash.subarray(32, 48);
+    const cipher = aes_encrypt(key, iv, $(elem).html());
+    $(elem).replaceWith(`
+    <div class="encrypt card" style="text-align:center;">
+      <div style="font-size:1.1rem;">🔐 内容受密码保护</div>
+      <p style="font-size:0.8rem;">密码提示：${hint}</p>
+      <form class="search-form" onsubmit="decrypt(this.querySelector('a'));return false;">
+        <input type="password" class="password search-input" placeholder="请输入密码" />
+        <input type="hidden" class="cipher" value="${cipher}" />
+        <a onclick="decrypt(this)" style="margin-left: .35em; margin-right: .35em">解密</a>
+      </form>
+    </div>
+    `);
+  });
+  data.content = $.html();
   if (data.content.includes(`class="encrypt card"`)) {
     data.content += `
     <script>
