8d61ba216daa028de5d5218d3a5bbaab51a80050
hans362 <i@hans362.cn>
2025-04-11 18:24:27 +0800

    improve encryption frontend

--- scripts/filters/encrypt.js
+++ scripts/filters/encrypt.js
@@ -24,14 +24,15 @@ hexo.extend.filter.register("after_post_render", (data) => {
     const key = hash.subarray(0, 32);
     const iv = hash.subarray(32, 48);
     const cipher = aes_encrypt(key, iv, $(elem).html());
+    const lock = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-lock"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2a5 5 0 0 1 5 5v3a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-10a3 3 0 0 1 -3 -3v-6a3 3 0 0 1 3 -3v-3a5 5 0 0 1 5 -5m0 12a2 2 0 0 0 -1.995 1.85l-.005 .15a2 2 0 1 0 2 -2m0 -10a3 3 0 0 0 -3 3v3h6v-3a3 3 0 0 0 -3 -3" /></svg>`;
     $(elem).replaceWith(`
     <div class="encrypt card" style="text-align:center;">
-      <div style="font-size:1.1rem;">🔐 内容受密码保护</div>
+      <div style="font-size:1.1rem;display:inline-flex;align-items:center;">${lock} 内容受密码保护</div>
       <p style="font-size:0.8rem;">密码提示：${hint}</p>
       <form class="search-form" onsubmit="decrypt(this.querySelector('a'));return false;">
-        <input type="password" class="password search-input" placeholder="请输入密码" />
+        <input type="password" class="password search-input" placeholder="请输入密码" autocomplete="password" />
         <input type="hidden" class="cipher" value="${cipher}" />
-        <a onclick="decrypt(this)" style="margin-left: .35em; margin-right: .35em">解密</a>
+        <a onclick="decrypt(this)" style="margin-left:.35em;margin-right:.35em">解密</a>
       </form>
     </div>
     `);
